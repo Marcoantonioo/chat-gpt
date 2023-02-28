19724b35a87f28a500c213fec5abd5c7a2a30862
Marco Antonio Lentz <marco.lentz2@gmail.com>
2023-02-28 17:20:48 -0300

    doc: added readme

--- app/src/main/java/com/example/chatgpt/commons/utils/DateExt.kt
+++ app/src/main/java/com/example/chatgpt/commons/utils/DateExt.kt
@@ -3,8 +3,8 @@ package com.example.chatgpt.commons.utils
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Long.formatDateToHourFormat() =
-    SimpleDateFormat("MMMM dd", Locale.getDefault()).format(Date(this))
+fun Long.formatDateToHourFormat(): String =
+    SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date(this))
 
-fun Long.formatDateToMonthAndDayFormat() =
+fun Long.formatDateToMonthAndDayFormat(): String =
     SimpleDateFormat("MMMM dd", Locale.getDefault()).format(Date(this))
